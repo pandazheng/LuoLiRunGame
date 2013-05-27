fb6b6b067e17e91d5ea77c77d4acee59d1b21e22
panda zheng <pandazheng@pandas-MacBook-Pro.local>
2013-05-27 08:49:09 -0700

    Initial Commit

---
+++ LuoLiRun/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  LuoLiRunAppController.h
+//  LuoLiRun
+//
+//  Created by panda zheng on 13-5-27.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ LuoLiRun/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  LuoLiRunAppController.h
+//  LuoLiRun
+//
+//  Created by panda zheng on 13-5-27.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
